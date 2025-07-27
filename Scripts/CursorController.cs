8886809b0142ff928a809651758fe902d92ddc73
SonarBeserk <sonarbeserk@gmail.com>
2025-07-26 23:15:37 -0400

    Scale up textures to 32x32 and adjust grid size

--- Scripts/CursorController.cs
+++ Scripts/CursorController.cs
@@ -12,12 +12,12 @@ public partial class CursorController : Node2D {
     }
 
     public override void _PhysicsProcess(double delta) {
-        GlobalPosition = GetWorldPosTile(GetGlobalMousePosition()) + new Vector2(8, 8);
+        GlobalPosition = GetWorldPosTile(GetGlobalMousePosition()) + new Vector2(16, 16);
     }
 
     public static Vector2I GetWorldPosTile(Vector2 worldPos) {
-        int xInt = Mathf.FloorToInt(worldPos.X / 16) * 16;
-        int yInt = Mathf.FloorToInt(worldPos.Y / 16) * 16;
+        int xInt = Mathf.FloorToInt(worldPos.X / 32) * 32;
+        int yInt = Mathf.FloorToInt(worldPos.Y / 32) * 32;
         Vector2I result = new(xInt, yInt);
         return result;
     }
